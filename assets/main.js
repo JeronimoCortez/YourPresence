4530bba695a00b9871b638667e6ae1d1a81ae2e3
tomas <tomascostapp@gmail.com>
2023-01-25 22:41:15 -0300

    Detalles

--- assets/main.js
+++ assets/main.js
@@ -23,8 +23,6 @@ window.onscroll = function() {
     }
         
     
-
-
     /* FORMAS */
     var posicion = window.pageYOffset || document.documentElement.scrollTop;
 
@@ -32,17 +30,9 @@ window.onscroll = function() {
     var element2 = document.getElementById('trabajosShape');
     var element3 = document.getElementById('contactoShape');
 
-    // var element4 = document.getElementById('nosotrosShape1');
-    // var element5 = document.getElementById('nosotrosShape2');
-    // var element6 = document.getElementById('nosotrosShape3');
-
     element1.style.top = posicion * 0.1 + "px";
     element2.style.top = posicion * 0.15 + "px";
     element3.style.bottom = posicion * 0.25 + "px";
-
-    // element4.style.bottom = posicion * 0.1 + "px";
-    // element5.style.top = posicion * 0.1 + "px";
-    // element6.style.bottom = posicion * 0.1 + "px";
 
 }
 
